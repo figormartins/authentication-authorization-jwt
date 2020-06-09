d5a755be96fe62fd0cff5cef9c5a6beb752fa239
Francisco Igor Teixeira Martins <igor.martins@grendene.com.br>
2020-06-09 14:29:43 -0300

    Example authentication.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+using Shop.Repositories;
+using Shop.Services;
+
+namespace Shop.Controllers
+{
+    [Route("v1/account")]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            // Recupera o usuário
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            // Verifica se o usuário existe
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            // Gera o Token
+            var token = TokenService.GenerateToken(user);
+
+            // Oculta a senha
+            user.Password = "";
+
+            // Retorna os dados
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,6 @@
+namespace Shop
+{
+    public static class Settings {
+        public static string Secret = "9395C9484385E7CC8CCE1B324E155A0CFA17A335E576F70CD8C91EC1AB265478";
+    }
+}
